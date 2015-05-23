8b7a57ef363be9f5fec0b083ffff8d214594c87f
Yen168 <yenan.chen@baruchmail.cuny.edu>
2015-05-23 00:14:28 -0400

    Create my_sum.rb

---
+++ my_sum.rb
@@ -0,0 +1,22 @@
+class MyArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def sum(initial_value = 0)
+  	
+  	return @array.inject(:+) +  initial_value unless block_given?
+
+  	sum = initial_value
+  	@array.each {|x| sum += yield(x)}
+  	sum
+
+  end
+end
+
+my_array = MyArray.new([1, 2, 3])
+puts my_array.sum(0) {|n| n**2}
+my_array.array = [1,3,3]
+
