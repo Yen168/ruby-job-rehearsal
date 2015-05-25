111a3d5df454b86e7e1b84aebb37cf13b44f4a3c
Yen168 <yenan.chen@baruchmail.cuny.edu>
2015-05-25 16:44:33 -0400

    Update kaprekar.rb

--- kaprekar.rb
+++ kaprekar.rb
@@ -1,6 +1,9 @@
 def kaprekar?(k)
+  
   n = k.to_s.size
   square = k**2
-  return false unless k == (square%10**n + square/10**n)
+  times = 10**n
+  
+  return false unless k == (square%times + square/times)
   true
 end
