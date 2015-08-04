c5a7dca06d8a3e120d4f4b3c7a65ecd3fd976683
¥en 円 <yenan.chen@baruchmail.cuny.edu>
2015-08-04 09:32:15 -0400

    Create fun.rb

---
+++ fun.rb
@@ -0,0 +1,32 @@
+puts ->(){
+
+	add_maker = ->(x){
+
+		->(y){x+y}
+	}
+
+	mul_maker = ->(x){
+
+		->(y){x*y}
+
+	}
+
+	con_fun = ->(x,y){
+
+		->(g){x[y[g]]}
+
+	}
+
+	
+	add1 = add_maker[1]
+	add1[2]
+
+	mul3 = mul_maker[3]
+	mul3[2]
+
+	add1mul3 = con_fun[mul3,add1]
+	add1mul3[2]
+
+	
+
+	}.()
